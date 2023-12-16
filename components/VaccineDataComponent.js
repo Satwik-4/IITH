5d919e13f63a04d0b1560c5382ed18b7571b0974
“Satwik <ep20btech11027@iith.ac.in>
2023-12-16 17:15:47 +0530

    First Commit

---
+++ components/VaccineDataComponent.js
@@ -0,0 +1,12 @@
+// components/VaccineDataComponent.js
+import React from 'react';
+
+const VaccineDataComponent = () => {
+  return (
+    <html lang="en">
+      {/* Your existing HTML code goes here */}
+    </html>
+  );
+};
+
+export default VaccineDataComponent;
