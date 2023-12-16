f829991a398e75c968e4fd10d4702ffd74d551ca
“Satwik <ep20btech11027@iith.ac.in>
2023-12-16 18:03:33 +0530

    Icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+// pages/_app.js
+import React from 'react';
+import Head from 'next/head';
+import '../styles/globals.css'; // Import global styles if you have any
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/IITH.ico" />
+        {/* Add other meta tags or links as needed */}
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
